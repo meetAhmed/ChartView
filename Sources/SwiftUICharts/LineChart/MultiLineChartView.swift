3b6b751df77477a92e6d6778807bc4b385657a03
Ahmed Ali <aali.97@yahoo.com>
2023-07-07 11:36:57 +0500

    Fixed issue MultiLineChartView Rate #263: If the rateValue is absent, the chart will not display the rateValue and percentage.

--- Sources/SwiftUICharts/LineChart/MultiLineChartView.swift
+++ Sources/SwiftUICharts/LineChart/MultiLineChartView.swift
@@ -85,13 +85,15 @@ public struct MultiLineChartView: View {
                                 .font(.callout)
                                 .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor : self.style.legendTextColor)
                         }
-                        HStack {
-                            if (rateValue ?? 0 >= 0){
-                                Image(systemName: "arrow.up")
-                            }else{
-                                Image(systemName: "arrow.down")
+                        if let rateValue {
+                            HStack {
+                                if (rateValue >= 0){
+                                    Image(systemName: "arrow.up")
+                                }else{
+                                    Image(systemName: "arrow.down")
+                                }
+                                Text("\(rateValue)%")
                             }
-                            Text("\(rateValue ?? 0)%")
                         }
                     }
                     .transition(.opacity)
